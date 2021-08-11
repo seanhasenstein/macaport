f7fb1b47f06e9455744d8ceadaf2c6ad1af036ca
Sean Hasenstein <seanhasenstein@gmail.com>
2021-08-11 14:12:33 -0500

    update orders db api

--- db/order.ts
+++ db/order.ts
@@ -1,35 +1,6 @@
 import { Db, ObjectID } from 'mongodb';
 import { Order, Store } from '../interfaces';
 
-export async function addOrder(db: Db, order: Order) {
-  try {
-    const result = await db.collection('orders').insertOne(order);
-
-    return result.ops[0];
-  } catch (error) {
-    console.error(error);
-    throw new Error('An error occurred while creating the order.');
-  }
-}
-
-export async function getOrder(db: Db, id: string) {
-  try {
-    const result = await db.collection('orders').findOne({ orderId: id });
-
-    if (!result) throw new Error('Invalid order # provided.');
-
-    return {
-      ...result,
-      _id: result._id.toString(),
-      createdAt: result.createdAt.toString(),
-      updatedAt: result.updatedAt.toString(),
-    };
-  } catch (error) {
-    console.error(error);
-    throw new Error('An error occurred while querying for the order.');
-  }
-}
-
 export async function getOrderFromStore(
   db: Db,
   storeId: string,
