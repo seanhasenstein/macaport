ddb74ac0ec246d496a812f71b6cc3bf41f8cd19b
Sean Hasenstein <seanhasenstein@gmail.com>
2022-04-26 14:23:38 -0500

    fix css bug with product page colors on small screens

--- pages/store/[id]/product.tsx
+++ pages/store/[id]/product.tsx
@@ -651,9 +651,10 @@ const ProductStyles = styled.div`
 
   .colors {
     .grid {
-      max-width: 18rem;
-      width: 100%;
       display: flex;
+      gap: 0.625rem;
+      flex-wrap: wrap;
+      width: 100%;
       list-style-type: none;
     }
   }
@@ -902,10 +903,7 @@ const ProductStyles = styled.div`
 `;
 
 const ColorStyles = styled.div`
-  margin: 0 0.875rem 0 0;
   position: relative;
-  height: 2.5rem;
-  width: 2.5rem;
 
   .label-wrapper {
     height: 2.5rem;
