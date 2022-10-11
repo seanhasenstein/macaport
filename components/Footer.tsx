1878a3561f581edcdfa35176dc60458f87a1bb22
Sean Hasenstein <seanhasenstein@gmail.com>
2022-10-10 21:40:56 -0500

    revert back to original storeLayout footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import styled from 'styled-components';
 
 export default function Footer() {
@@ -6,15 +5,6 @@ export default function Footer() {
     <FooterStyles>
       <div className="footer-wrapper">
         <p>&copy; Macaport {new Date().getFullYear()}. All Rights Reserved.</p>
-        <div className="nav">
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <div className="divider" arria-hidden="true" />
-          <Link href="/terms-and-conditions">Terms &amp; conditions</Link>
-          <div className="divider" arria-hidden="true" />
-          <Link href="/privacy-policy">Privacy policy</Link>
-        </div>
       </div>
     </FooterStyles>
   );
@@ -26,86 +16,20 @@ const FooterStyles = styled.footer`
   width: 100%;
   text-align: center;
 
-  p {
-    margin: 0;
-    color: #6b7280;
-    font-size: 0.875rem;
-  }
-
   .footer-wrapper {
     margin: 0 auto;
-    padding: 2.5rem 0;
-    max-width: 72rem;
+    padding: 2rem 0;
+    max-width: 69rem;
     width: 100%;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-top: 1px solid #d1d5db;
-  }
-
-  .nav {
-    display: flex;
     justify-content: center;
-
-    a {
-      font-size: 0.875rem;
-      font-weight: 600;
-      color: #334155;
-      transition: color 100ms linear;
-
-      &:hover {
-        color: #0f172a;
-      }
-    }
+    align-items: center;
+    border-top: 1px solid #c6cbd2;
   }
 
-  .divider {
-    margin: 0 1.75rem;
-    height: 1rem;
-    width: 1px;
-    background-color: #cbd5e1;
-  }
-
-  @media (max-width: 768px) {
-    .footer-wrapper {
-      flex-direction: column;
-    }
-
-    .nav {
-      margin: 3rem 0 0;
-    }
-  }
-
-  @media (max-width: 640px) {
-    .footer-wrapper {
-      padding-top: 0;
-      flex-direction: column-reverse;
-      border-top: none;
-    }
-
-    .nav {
-      margin: 0 0 3rem;
-      flex-direction: column;
-      text-align: center;
-      width: 100%;
-      background-color: #fff;
-      border-top: 1px solid #e2e8f0;
-      border-radius: 0.25rem;
-      box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
-
-      a {
-        padding: 1rem 0;
-        width: 100%;
-        border-bottom: 1px solid #e2e8f0;
-
-        &:last-of-type {
-          border-bottom: none;
-        }
-      }
-    }
-
-    .divider {
-      display: none;
-    }
+  p {
+    margin: 0;
+    color: #595f6b;
+    font-size: 0.875rem;
   }
 `;
