8fe7ba2ec6770aaa2cc5c49da00a29cb333ee89b
Sean Hasenstein <seanhasenstein@gmail.com>
2023-03-04 21:40:11 -0600

    update demo store banner content and styles

--- components/store/demo/banner.tsx
+++ components/store/demo/banner.tsx
@@ -4,30 +4,38 @@ export default function DemoBanner() {
   return (
     <DemoBannerStyles>
       <div className="banner-container">
-        <p>This is a demo (the store is currently closed)</p>
+        <p>This is a demo store</p>
       </div>
     </DemoBannerStyles>
   );
 }
 
 const DemoBannerStyles = styled.div`
-  padding: 1rem 2rem;
-  position: fixed;
-  left: 1.5rem;
-  bottom: 1.5rem;
-  width: calc(100% - 3rem);
+  padding: 0.75rem 2rem 0.6875rem;
+  position: absolute;
+  left: calc(50% - 10rem);
+  top: 5.25rem;
+  width: 20rem;
   display: flex;
   justify-content: center;
-  background-color: rgba(38, 83, 53, 0.95);
-  border-radius: 0.5rem;
+  background-color: rgba(38, 83, 53, 0.93);
+  border: 3px solid rgba(38, 83, 53, 1);
+  border-radius: 0 0 0.625rem 0.625rem;
   box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
   z-index: 9999;
 
   p {
     margin: 0;
-    font-weight: 500;
+    font-size: 0.875rem;
+    font-weight: 600;
     color: #fff;
     text-align: center;
     line-height: 1.5;
+    text-transform: uppercase;
+    letter-spacing: 0.1em;
+  }
+
+  @media (max-width: 500px) {
+    top: 4.75rem;
   }
 `;
