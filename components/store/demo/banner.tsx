89e6df35b59f95107f5ee051381d430c7fba7ce6
Sean Hasenstein <seanhasenstein@gmail.com>
2025-01-19 19:26:36 -0600

    update banner for demo stores

--- components/store/demo/banner.tsx
+++ components/store/demo/banner.tsx
@@ -1,6 +1,15 @@
+import React from 'react';
 import styled from 'styled-components';
 
 export default function DemoBanner() {
+  React.useEffect(() => {
+    document.body.style.paddingTop = '2.625rem';
+
+    return () => {
+      document.body.style.paddingTop = '0';
+    };
+  }, []);
+
   return (
     <DemoBannerStyles>
       <div className="banner-container">
@@ -11,31 +20,26 @@ export default function DemoBanner() {
 }
 
 const DemoBannerStyles = styled.div`
-  padding: 0.75rem 2rem 0.6875rem;
+  padding: 0;
   position: absolute;
-  left: calc(50% - 10rem);
-  top: 5.25rem;
-  width: 20rem;
+  left: 0;
+  top: 0;
+  height: 2.625rem;
+  width: 100%;
   display: flex;
+  align-items: center;
   justify-content: center;
-  background-color: rgba(38, 83, 53, 0.93);
-  border: 3px solid rgba(38, 83, 53, 1);
-  border-radius: 0 0 0.625rem 0.625rem;
-  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+  background-color: #265335;
   z-index: 9999;
 
   p {
     margin: 0;
-    font-size: 0.875rem;
+    font-size: 0.8125rem;
     font-weight: 600;
-    color: #fff;
+    color: #f4faf6;
     text-align: center;
-    line-height: 1.5;
+    line-height: 1;
     text-transform: uppercase;
-    letter-spacing: 0.1em;
-  }
-
-  @media (max-width: 500px) {
-    top: 4.75rem;
+    letter-spacing: 0.2em;
   }
 `;
