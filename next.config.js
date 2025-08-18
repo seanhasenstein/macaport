3d657a5d490456f18fbc155f8c34850f9a18fc11
Sean Hasenstein <seanhasenstein@gmail.com>
2025-08-18 13:35:05 -0500

    add lhscc temp redirect to next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ module.exports = {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/lhscc',
+        destination: '/store/689f2b9aab35069ae78bb4e9',
+        permanent: false, // This makes it a temporary redirect (302)
+      },
+    ];
+  },
 };
